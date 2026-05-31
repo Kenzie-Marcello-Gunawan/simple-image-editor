0f4b022e6ce5d942c879d7837317c32f6dc19ee2
Kenzie-Marcello-Gunawan <ronnysiaulung@gmail.com>
2026-05-31 11:28:21 +0700

    v1

---
+++ main.py
@@ -0,0 +1,145 @@
+from PyQt5.QtWidgets import (
+   QApplication, QWidget,
+   QFileDialog,
+   QLabel, QPushButton, QListWidget,
+   QHBoxLayout, QVBoxLayout
+)
+import os
+from PIL import Image
+from PIL import ImageFilter
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
+app = QApplication([])
+win = QWidget()       
+win.resize(700, 500) 
+win.setWindowTitle('Easy Editor')
+lb_image = QLabel("Image")
+btn_dir = QPushButton("Folder")
+lw_files = QListWidget()
+
+
+btn_left = QPushButton("Left")
+btn_right = QPushButton("Right")
+btn_flip = QPushButton("Mirror")
+btn_sharp = QPushButton("Sharpness")
+btn_bw = QPushButton("B/W")
+
+
+row = QHBoxLayout()         
+col1 = QVBoxLayout()         
+col2 = QVBoxLayout()
+col1.addWidget(btn_dir)      
+col1.addWidget(lw_files)     
+col2.addWidget(lb_image, 95) 
+row_tools = QHBoxLayout()    
+row_tools.addWidget(btn_left)
+row_tools.addWidget(btn_right)
+row_tools.addWidget(btn_flip)
+row_tools.addWidget(btn_sharp)
+row_tools.addWidget(btn_bw)
+col2.addLayout(row_tools)
+
+
+row.addLayout(col1, 20)
+row.addLayout(col2, 80)
+win.setLayout(row)
+
+workdir = ''
+
+def filter(files, extensions):
+   result = []
+   for Filename in files:
+      for ext in extensions:
+         if Filename.endswith(ext):
+            result.append(Filename)
+   return result
+
+def chooseWorkdir():
+   global workdir
+   workdir = QFileDialog.getExistingDirectory()
+
+def showFilenamesList():
+   extensions =['.jpg', '.png', '.jpeg', '.gif', '.bmp']
+   chooseWorkdir()
+   filenames = filter(os.listdir(workdir), extensions)
+   lw_files.clear()
+   for filename in filenames:
+      lw_files.addItem(filename)
+
+btn_dir.clicked.connect(showFilenamesList)
+
+class ImageProcessor():
+   def __init__(self):
+      self.image = None
+      self.dir = None
+      self.filename = None 
+      self.save_dir = "Modified/"
+
+   def loadImage(self, dir, filename):
+      self.dir = dir
+      self.filename = filename
+      image_path = os.path.join(dir, filename)
+      self.image = Image.open(image_path)
+
+   def showImage(self, path):
+      lb_image.hide()
+      pixmapimage = QPixmap(path)
+      w, h = lb_image.width(), lb_image.height()
+      pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+      lb_image.setPixmap(pixmapimage)
+      lb_image.show()
+
+   def do_bw(self):
+      self.image = self.image.convert("L")
+      self.saveImage()
+      image_path = os.path.join(self.dir, self.save_dir, self.filename)
+      self.showImage(image_path)
+
+   def do_left(self):
+      self.image = self.image.transpose(Image.ROTATE_90)
+      self.saveImage()
+      image_path = os.path.join(self.dir, self.save_dir, self.filename)
+      self.showImage(image_path)
+
+   def do_right(self):
+      self.image = self.image.transpose(Image.ROTATE_270)
+      self.saveImage()
+      image_path = os.path.join(self.dir, self.save_dir, self.filename)
+      self.showImage(image_path)
+   def mirror(self):
+      self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+      self.saveImage()
+      image_path = os.path.join(self.dir, self.save_dir, self.filename)
+      self.showImage(image_path)
+
+   def sharpen(self):
+      self.image = self.image.filter(ImageFilter.SHARPEN)
+      self.saveImage()
+      image_path = os.path.join(self.dir, self.save_dir, self.filename)
+      self.showImage(image_path)
+
+   def saveImage(self):
+      path = os.path.join(self.dir, self.save_dir)
+      if not(os.path.exists(path) or os.path.isdir(path)):
+         os.mkdir(path)
+      image_path = os.path.join(path, self.filename)
+      self.image.save(image_path)
+
+workimage = ImageProcessor()
+
+def showChosenImage():
+   if lw_files.currentRow() >= 0:
+      filename = lw_files.currentItem().text()
+      workimage.loadImage(workdir, filename)
+      image_path = os.path.join(workimage.dir, workimage.filename)
+      workimage.showImage(image_path)
+
+lw_files.currentRowChanged.connect(showChosenImage)
+btn_bw.clicked.connect(workimage.do_bw)
+btn_left.clicked.connect(workimage.do_left)
+btn_right.clicked.connect(workimage.do_right)
+btn_flip.clicked.connect(workimage.mirror)
+btn_sharp.clicked.connect(workimage.sharpen)
+win.show()
+app.exec()
